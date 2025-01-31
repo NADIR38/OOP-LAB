b9d67fdef0316cd62a273159a2f4735929ba0113
NADIR38 <jamalnadir7778@gmail.com>
2025-01-31 19:05:28 +0500

    calculator.cs

--- week/ConsoleApp2/Program.cs
+++ week/ConsoleApp2/Program.cs
@@ -10,6 +10,56 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+        calculator calc = new calculator();
+            int x, y;
+            int choice = mainmenu();
+            while (true)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the first number");
+                        x=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y=int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Addition: x+y= {calc.add(x,y)}");
+                        break;
+                        case 2:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"subtraction: x-y = {calc.subtract(x,y)}");
+                        break;
+                        case 3:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"multiplication: x*y= {calc.multiply(x,y)}");
+                        break;
+                        case 4:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"division: x/y = {calc.divide(x,y)}"); 
+                        break;
+                }
+                choice = mainmenu();
+            }
+        }
+        public static int mainmenu()
+        {
+            Console.WriteLine("basic calculator");
+
+            Console.WriteLine("ADDITION");
+            Console.WriteLine("subtraction");
+            Console.WriteLine(("multiplication"));
+            Console.WriteLine("division");
+            int option;
+            option=int.Parse(Console.ReadLine());
+            return option;
         }
     }
 }
---
+++ week/ConsoleApp2/calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public  class calculator
+    {
+        public calculator()
+        { 
+        }
+        public float add(int x,int y) 
+        {
+        return x + y;
+        }
+        public float subtract(int x, int y) 
+        {
+        return x - y;
+        }
+        public float multiply(int x, int y)
+        { 
+        return x * y;   
+        }
+        public float divide(int x, int y) 
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("numbers not divisble");
+                return 0;
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+
+    }
+}
