7b876fbb8b781a5260b7ff7f79e800d1955f0f8c
NADIR38 <jamalnadir7778@gmail.com>
2025-01-31 20:30:47 +0500

    atm.cs

---
+++ week/ATM/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            atm str = new atm(10000.0f);
+           
+            int choice = mainmenu();
+            float amount;
+            while (true)
+            {
+                switch (choice)
+                {
+                    case 1:
+
+                        Console.WriteLine($"Your balance is {str.Amount()}");
+
+                        break;
+                        case 2:
+                        Console.WriteLine("enter the amount yoyu want to deposit");
+                        amount=int.Parse(Console.ReadLine());
+                        str.deposit(amount);
+                        break;
+                    case 3:
+                        Console.WriteLine("enter the amount yoyu want to withdraw");
+                        amount = int.Parse(Console.ReadLine());
+                        str.withdraw(amount);
+                        break;
+                    case 4:
+                        str.transactions();
+                        break;
+                }
+                choice = mainmenu();
+
+            }
+        }
+        static int mainmenu()
+        {
+            Console.WriteLine("ATM");
+                Console.WriteLine("Check balance");
+            Console.WriteLine("Deposit Amount");
+            Console.WriteLine("Withdraw Amount");
+            Console.WriteLine("View transcation history");
+            int option=int.Parse(Console.ReadLine());
+            return option;
+        }
+    }
+}
---
+++ week/ATM/atm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public  class atm
+    {
+        public float balance;
+        public List<string> transaction;
+        public atm(float Balance)
+        {
+            balance = Balance;
+            transaction = new List<string>();
+
+        }
+        public float Amount()
+
+        {
+            return balance;
+        }
+        public void deposit(float amount)
+        {
+            balance += amount;
+            transaction.Add($"{DateTime.Now} - deposited {amount}");
+            Console.WriteLine($"Deposited:{amount}");
+            Console.WriteLine($"total balance:{balance}");
+        }
+        public void withdraw(float amount)
+        {
+            if (balance < 0 || amount > balance)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else
+            {
+                balance -= amount;
+                transaction.Add($"{DateTime.Now} - withdrawn {amount}");
+
+                Console.WriteLine($"Withdrawn amount:{amount}");
+                Console.WriteLine($"remaining balance balance:{balance}");
+
+            }
+
+        }
+        public void transactions()
+        {
+            foreach (var transaction in transaction) 
+                {
+                Console.WriteLine(transaction);
+                }
+        }
+
+    }
+}
+  
